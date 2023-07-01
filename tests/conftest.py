51fd7a87665020d274f81d1d248162d9ab765bcf
Polina Bezrukhih <polybezrukhih@gmail.com>
2023-07-01 23:10:02 +0700

    1

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ sys.path.append(root_dir)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 root_dir_content = os.listdir(BASE_DIR)
+
 PROJECT_DIR_NAME = 'yatube_api'
 
 if (
